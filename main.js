d4a4173e10fcb485bac96e9c0bfc0bd15033586e
Sweet-choc142 <98330272+Sweet-choc142@users.noreply.github.com>
2022-03-13 20:02:58 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var images = ["https://i.postimg.cc/MGn9GJXw/family.jpg","https://i.postimg.cc/qqyYvVbq/grandpa.jpg","https://www.nicepng.com/png/detail/300-3001684_cartoon-grandma-clipart-grandma-clipart-black-and-white.png", "https://i.postimg.cc/wjMnFtMX/father.jpg" , "https://i.postimg.cc/5ymDKL83/bro.jpg", "https://i.postimg.cc/JnL6wtrd/sister.jpg", "https://i.postimg.cc/bw5W5zSK/mother.jpg"];
-var names = ["Fmaily Book","Chakradhara Pani", "Bijanti Pani","Chittaranjan Pani", "Chirantan Pani", "Chetana Pani", "Monalisa Mishra"];
+var names = ["Family Book","Chakradhara Pani", "Bijanti Pani","Chittaranjan Pani", "Chirantan Pani", "Chetana Pani", "Monalisa Mishra"];
 var i = 0;
 function update()
 {
